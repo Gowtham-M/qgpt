6f6924b211d863fa61bdb423fb29d3f8b7e560c2
Gowtham <chmgowtham@gmail.com>
2025-04-30 12:05:17 +0530

    removed offline access

--- everi_ai_qgpt_core/server/onedrive/onedrive_client.py
+++ everi_ai_qgpt_core/server/onedrive/onedrive_client.py
@@ -39,7 +39,7 @@ class OneDriveClient:
     def __init__(self, token_file="/home/srikar/QGPT_BE/QGPT/everi_ai_qgpt_core/tokenFolder/tokens.json"):
         self.CLIENT_ID = "7126642c-5253-4fc7-8ba5-96ae765a4bd9"  # Replace with your actual client ID
         self.AUTHORITY = "https://login.microsoftonline.com/common"
-        self.SCOPES = ["Files.Read", "offline_access"]  # Added offline_access for refresh tokens
+        self.SCOPES = ["Files.Read"]  # Added offline_access for refresh tokens
         self.access_token = None
         self.refresh_token = None
         self.token_file = token_file
